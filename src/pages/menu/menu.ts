3ff4f1d5163ab34f018e2b77a206bd7c87c2cf02
Edgar Sandoval <edgarsandovalavalos@hotmail.com>
2018-02-15 02:35:17 -0600

    Maquetado parcial de vista en el menú.

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { HomePage } from '../home/home';
 /**
  * Generated class for the MenuPage page.
  *
@@ -14,12 +14,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'menu.html',
 })
 export class MenuPage {
+    rootPage: any = HomePage;
+    pages: Array<{title: string, component: any, icon: string}>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
+        this.pages = [
+            { title: 'Inicio', component: HomePage, icon: 'home'},
+            { title: 'Red', component: HomePage, icon: 'people'},
+            { title: 'Compra Agua', component: HomePage, icon: 'water'},
+            { title: 'Mapa máquinas', component: HomePage, icon: 'pin'},
+            { title: 'Mis máquinas', component: HomePage, icon: 'build'},
+            { title: 'Mi perfil', component: HomePage, icon: 'person'},
+            { title: 'Ayuda', component: HomePage, icon: 'help-circle'},
+        ];
+    }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad MenuPage');
-  }
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad MenuPage');
+    }
+
+    openPage(page) {
+        // Reset the content nav to have just this page
+        // we wouldn't want the back button to show in this scenario
+        this.rootPage = page;
+    }
 
 }
